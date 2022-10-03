33baecef9a89c3b361b7897bf1c7c432e5efe162
ratara5 <ratara5@gmail.com>
2022-10-03 19:59:44 +0000

    First commit on development

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["ratara5/invoices/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"ratara5/invoices/test/unit/controller/MainView.controller"
+], function () {
+	"use strict";
+});
